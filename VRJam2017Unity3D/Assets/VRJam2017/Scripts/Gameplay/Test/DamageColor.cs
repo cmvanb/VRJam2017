3e03bc59b06623f271d2c72568c80e132dbe8d13
David Goemans <dgoemans@gmail.com>
2017-09-23 12:35:29 +0200

    Minion kill test

---
+++ VRJam2017Unity3D/Assets/VRJam2017/Scripts/Gameplay/Test/DamageColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Damage(float amount)
+	{
+		StartCoroutine(this.changeColor());
+	}
+
+	IEnumerator changeColor()
+	{
+		GetComponentInChildren<Renderer>().material.color = Color.red;
+
+		yield return new WaitForSeconds(0.1f);
+
+		GetComponentInChildren<Renderer>().material.color = Color.white;
+	}
+}
