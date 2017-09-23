1213370400fdaad6437fc890450804c1605ec74f
Casper <cmvanb@gmail.com>
2017-09-23 19:46:31 +0200

    player spawn position

--- VRJam2017Unity3D/Assets/VRJam2017/Scripts/Gameplay/GameState/GameManager.cs
+++ VRJam2017Unity3D/Assets/VRJam2017/Scripts/Gameplay/GameState/GameManager.cs
@@ -1,21 +1,36 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using AltSrc.UnityCommon.Patterns;
+using VRTK;
 
 public class GameManager : MonoSingleton<GameManager>
 {
     public Vector3 SpawnPosition;
-    public GameObject PlayerCameraRig;
+    public Transform PlayArea;
+    public Transform Headset;
 
     public void Start()
     {
+        PlayArea = VRTK_DeviceFinder.PlayAreaTransform();
+        Headset = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.Headset);
+
         SpawnPlayer();
     }
 
     public void SpawnPlayer()
     {
-        float y = LevelHelpers.GetTerrainHeightAtWorldPos(SpawnPosition);
+        SetPlayerPosition(SpawnPosition);
+    }
 
-        PlayerCameraRig.transform.position = new Vector3(SpawnPosition.x, y, SpawnPosition.z);
+    public void SetPlayerPosition(Vector3 position)
+    {
+        // NOTE: HACK - COULDNT FIGURE OUT HOW TO CALCULATE EXACT PLAYER HEIGHT IN VRTK
+        float hackOffset = 1.15f;
+
+        float playerHeightOffset = hackOffset;
+
+        float terrainHeight = LevelHelpers.GetTerrainHeightAtWorldPos(SpawnPosition);
+
+        PlayArea.position = new Vector3(position.x, terrainHeight + playerHeightOffset, position.z);
     }
 }
---
+++ VRJam2017Unity3D/Assets/VRJam2017/Scripts/VRTK/VRTK_Accessor.cs
@@ -0,0 +1,11 @@
+using AltSrc.UnityCommon.Patterns;
+using VRTK;
+
+public class VRTK_Accessor : MonoSingleton<VRTK_Accessor>
+{
+    public VRTK_BasicTeleport Teleporter;
+
+    public void Start()
+    {
+    }
+}
