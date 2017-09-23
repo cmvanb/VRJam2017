80b3eb337f4d3a2a253eef6ed5e7c6f611bc761c
David Goemans <dgoemans@gmail.com>
2017-09-23 23:13:27 +0200

    Animations take 1

--- VRJam2017Unity3D/Assets/VRJam2017/Scripts/Gameplay/Minions/MinionAnimationState.cs
+++ VRJam2017Unity3D/Assets/VRJam2017/Scripts/Gameplay/Minions/MinionAnimationState.cs
@@ -2,18 +2,32 @@ using UnityEngine;
 
 public class MinionAnimationState : MonoBehaviour
 {
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        
+        animator.Play(GetIdle());
+    }
+
     void StartMoving()
     {
-        //Debug.Log("Started Moving");
+        animator.Play("walk");
     }
 
     void StopMoving()
     {
-        //Debug.Log("Stopped Moving");
+        animator.Play(GetIdle());
     }
 
     void Attack()
     {
         //Debug.Log("Attack");
+    }
+
+    string GetIdle()
+    {
+        return Random.value > 0.5 ? "idle" : "idle2";
     }
 }
