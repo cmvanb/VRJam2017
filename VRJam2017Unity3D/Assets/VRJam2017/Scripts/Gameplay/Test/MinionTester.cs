7f58ed49b4def981cefdcc6332433e672865437b
David Goemans <dgoemans@gmail.com>
2017-09-23 12:10:25 +0200

    Basic minion

---
+++ VRJam2017Unity3D/Assets/VRJam2017/Scripts/Gameplay/Test/MinionTester.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionTester : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Vector3 pos = new Vector3(10,0,10);
+		
+		UnitAction action = new TerrainAction(gameObject, pos);
+
+		GetComponent<ActionQueue>().Add(action);
+		
+		pos = new Vector3(-10,0,10);
+
+		action = new TerrainAction(gameObject, pos);
+
+		GetComponent<ActionQueue>().Add(action);
+
+		pos = new Vector3(0,0,0);
+
+		action = new TerrainAction(gameObject, pos);
+
+		GetComponent<ActionQueue>().Add(action);
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
