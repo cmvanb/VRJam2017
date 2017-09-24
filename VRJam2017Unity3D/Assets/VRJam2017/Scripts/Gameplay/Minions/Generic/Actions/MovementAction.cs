7dea75c6c785bcf37562308d8df2dbe0920a2b79
David Goemans <dgoemans@gmail.com>
2017-09-24 10:55:33 +0200

    Summoning now stops 3 away from the summoner

--- VRJam2017Unity3D/Assets/VRJam2017/Scripts/Gameplay/Minions/Generic/Actions/MovementAction.cs
+++ VRJam2017Unity3D/Assets/VRJam2017/Scripts/Gameplay/Minions/Generic/Actions/MovementAction.cs
@@ -5,9 +5,11 @@ using System.Collections;
 
 public class MovementAction : UnitAction
 {
-    NavMeshAgent agent;
+    protected NavMeshAgent agent;
 
     public Vector3 Position = Vector3.zero;
+
+    protected float stoppingDistance = 0;
 
 
     public MovementAction(GameObject owner, Vector3 position) : base(owner)
@@ -60,7 +62,7 @@ public class MovementAction : UnitAction
 
         bool validPath = (agent.pathStatus == NavMeshPathStatus.PathComplete || agent.pathStatus == NavMeshPathStatus.PathPartial);
 
-        return dist != Mathf.Infinity && validPath && dist == 0;
+        return dist != Mathf.Infinity && validPath && dist <= stoppingDistance;
     }
 
     public override void Stop()
