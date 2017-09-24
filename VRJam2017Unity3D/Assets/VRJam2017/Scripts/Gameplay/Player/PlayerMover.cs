7083cd9cd5abeb5eb1018f441dba594fd94d03ef
Casper <cmvanb@gmail.com>
2017-09-24 11:10:41 +0200

    refactor to player controls

---
+++ VRJam2017Unity3D/Assets/VRJam2017/Scripts/Gameplay/Player/PlayerMover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    // DASH
+    public void Dash()
+    {
+        Debug.LogWarning("DASH");
+    }
+}
